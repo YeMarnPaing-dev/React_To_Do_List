05a83fa97f95d2637c41c4fb609abccfc9074edd
Saw Ye Marn <yemarnpaing44@gmail.com>
2025-11-16 02:35:22 +0630

    UIFix

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 const Form = () => {
   return (
   <>
+  <h1 className='mt-3 text-white'>React Todo List Project</h1>
     <div className='row mt-5'>
-        <div className="col">
+        <div className="col-6 offset-3">
            <input type="text" className='form-control' placeholder='Enter tasks....'/>
         </div>
-        <div className="col">
-            <button className='btn btn-primary '>Add Tasks</button>
+        <div className="col-3">
+            <button className='btn btn-primary '><i class="fa-solid fa-plus"></i>Add Tasks</button>
         </div>
     </div>
   </>
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -8,12 +8,34 @@ const List = () => {
         <Form/>
        <div className='mt-3'>
          <ol className='list-group'>
-            <div className="list-group-item w-75">
+            <div className="list-group-item w-100 shadow-sm mb-2">
                <div className="row">
-                 <div className="col">Task Message</div>
-                <div className="col">Delete Icon</div>
+                 <div className="col-9 offset-1">To Do List Tasks Message</div>
+                <div className="col-2"><i class="fa-solid fa-trash"></i></div>
                </div>
-                </div>            
+                </div>    
+
+                  <div className="list-group-item w-100 shadow-sm mb-2">
+               <div className="row">
+                 <div className="col-9 offset-1">To Do List Tasks Message</div>
+                <div className="col-2"><i class="fa-solid fa-trash"></i></div>
+               </div>
+                </div>    
+
+                  <div className="list-group-item w-100 shadow-sm mb-2">
+               <div className="row">
+                 <div className="col-9 offset-1">To Do List Tasks Message</div>
+                <div className="col-2"><i class="fa-solid fa-trash"></i></div>
+               </div>
+                </div>    
+
+                  <div className="list-group-item w-100 shadow-sm mb-2">
+               <div className="row">
+                 <div className="col-9 offset-1">To Do List Tasks Message</div>
+                <div className="col-2"><i class="fa-solid fa-trash"></i></div>
+               </div>
+                </div>        
+           
         </ol>
        </div>
       
