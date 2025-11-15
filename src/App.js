ede18b6a36cac8ae179b67dfe1a00a711496b09b
Saw Ye Marn <yemarnpaing44@gmail.com>
2025-11-16 01:20:09 +0630

    ToDoLIstUI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import List from './components/List';
+
 
 function App() {
   return (
     <div className="App">
-      This is To Do List
+       <List/>
     </div>
   );
 }
---
+++ src/components/List.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from './Form'
+
+const List = () => {
+  return (
+    
+    <div>
+        <Form/>
+        List
+        <button className='btn btn-success'>Hello Boostrap</button>
+        </div>
+  )
+}
+
+export default List
