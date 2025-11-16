b2e69ab28c41ea4147f93bb304d97aac5848936d
Saw Ye Marn <yemarnpaing44@gmail.com>
2025-11-17 00:42:31 +0630

    useRef

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 const Form = () => {
+
+  const [userTask,setTask] = useState('')
+
+  const submitTask = () => {
+    setTask('')
+        
+  }
+
   return (
   <>
   <h1 className='mt-3 text-white'>React Todo List Project</h1>
     <div className='row mt-5'>
         <div className="col-6 offset-3">
-           <input type="text" className='form-control' placeholder='Enter tasks....'/>
+           <input value={userTask} onChange={ e=>setTask(e.target.value) }  type="text" className='form-control' placeholder='Enter tasks....'/>
         </div>
         <div className="col-3">
-            <button className='btn btn-primary '><i className="fa-solid fa-plus"></i>Add Tasks</button>
+            <button type='button' onClick={submitTask} className='btn btn-primary '><i className="fa-solid fa-plus"></i>Add Tasks</button>
         </div>
     </div>
   </>
